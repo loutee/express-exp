06a281c7aba65b73484e8ff4e7ae6b985387e8f1
Louis Tran <loutran987@gmail.com>
2017-04-16 01:31:25 -0700

    Add body-parser package to read form data
    Removed semi-colons in server.js as well.

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-/* jshint esversion: 6 */
-const express = require('express');
+/* jshint esversion: 6, asi: true */
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
 
   res.sendFile(__dirname + '/index.html');
 
-});
+})
 
 app.post('/abilities', (req, res) => {
 
-  console.log('POST request made');
+  console.log(req.body)
 
-});
+})
 
 app.listen(3000, () => {
 
-  console.log('listening on 3000');
+  console.log('listening on 3000')
 
-});
-
+})
