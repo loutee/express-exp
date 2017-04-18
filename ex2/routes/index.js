3b3fe8ef1c3263621b3d8db9a89f21d3a9047b5e
Louis Tran <loutran987@gmail.com>
2017-04-18 00:22:18 -0700

    Add pages and partials
    Pages include about, help, and contact.
    Partials include header and footer.

--- ex2/routes/index.js
+++ ex2/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Experimental Express App' });
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Experimental Express App' });
+});
+
+router.get('/help', function(req, res, next) {
+ res.render('help', { title: 'Experimental Express App' });
+})
+
+router.get('/contact', function(req, res, next) {
+ res.render('contact', { title: 'Experimental Express App' });
+})
 
 module.exports = router;
