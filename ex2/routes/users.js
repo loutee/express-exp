3789ffa6bfb69185d229b2d7a9ee5c91efa9055c
Louis Tran <loutran987@gmail.com>
2017-04-27 22:22:28 -0700

    Add post user action
    Functionality has not yet been verified to work.

--- ex2/routes/users.js
+++ ex2/routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+var User = mongoose.model('User', userSchema);
 
 /* GET new user form. */
-router.get('/new', function(req, res, next) {
+router.get('/new', function(req, res) {
   res.render('users/new');
+});
+
+/* POST new user form. */
+router.post('/new', function(req, res) {
+  var userInfo = req.body;
+  if(!userInfo.name || !userInfo.age) {
+    res.render('users/show_message', {message: "Field is blank", type: "error"});
+  } else {
+    var newUser = new User({
+      name: userInfo.name,
+      age: userInfo.name
+    });
+
+    newUser.save(function(err, User){
+      if(err)
+        res.render('users/show_message', {message: "Database error", type: "error"});
+      else
+        res.render('users/show_message', {message: "New user added", type: "success", user: userInfo});
+    });
+  }
 });
 
 module.exports = router;
