4d8d0d05f3c0d436b57173869ee45e9a31031b14
Louis Tran <loutran987@gmail.com>
2017-05-04 22:57:00 -0700

    Add admin page controller and template and fix bug
    Bug found in bin/www and is now resolved. Installed underscore npm
    package resulting in passing tests.

--- ex3/routes/index.js
+++ ex3/routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Admin = require('../controllers/Admin');
+
+var attachDB = function(req, res, next) {
+  req.db = db;
+  next();
+};
+
+router.all('/admin*', attachDB, function(req, res, next) {
+  Admin.run(req, res, next);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
