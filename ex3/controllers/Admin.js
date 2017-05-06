36417ecac52601ef7a6259f25b92589ebeb4a3a5
Louis Tran <loutran987@gmail.com>
2017-05-05 23:25:16 -0700

    Add admin page protection

--- ex3/controllers/Admin.js
+++ ex3/controllers/Admin.js
@@ -3,10 +3,33 @@ var BaseController = require("./Base"),
 module.exports = BaseController.extend({
   name: "Admin",
   run: function(req, res, next) {
-    var v = new View(res, 'admin');
-    v.render({
-      title: 'Administration',
-      content: 'Welcome to the control panel'
-    });
+    if(this.authorization(req)) {
+      req.session.expapp = true;
+      req.session.save(function(err) {
+        var v = new View(res, 'admin');
+        v.render({
+          title: 'Administration',
+          content: 'Welcome to the control panel'
+        });
+      });
+    } else {
+      var v = new View(res, 'admin-login');
+      v.render({
+        title: 'Please login'
+      });
+    }
+  },
+  authorize: function(req) {
+    return (
+      req.session &&
+      req.session.expapp &&
+      req.session.expapp === true
+    ) || (
+      req.body &&
+      req.body.username === this.username &&
+      req.body.password === this.password
+    );
   }
 });
+
+
