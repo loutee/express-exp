2297f82e3c6fca81180ffc730c7e4d8031ada78a
Louis Tran <loutran987@gmail.com>
2017-05-06 22:24:04 -0700

    Add content management

--- ex3/controllers/Admin.js
+++ ex3/controllers/Admin.js
@@ -1,19 +1,29 @@
-var BaseController = require("./Base"),
-    View = require("../views/Base");
+var BaseController = require("./Base");
+var View = require("../views/Base");
+var v;
 module.exports = BaseController.extend({
   name: "Admin",
   run: function(req, res, next) {
+    var self = this;
     if(this.authorization(req)) {
+      model.setDB(req.db);
       req.session.expapp = true;
-      req.session.save(function(err) {
-        var v = new View(res, 'admin');
-        v.render({
-          title: 'Administration',
-          content: 'Welcome to the control panel'
+      req.session.save();
+      v = new View(res, 'admin');
+      self.del(req, function() {
+        self.form(req, res, function(formMarkup) {
+          self.list(function(listMarkup) {
+            v.render({
+              title: 'Administration',
+              content: 'Welcome to the control panel',
+              list: listMarkup,
+              form: formMarkup
+            });
+          });
         });
       });
     } else {
-      var v = new View(res, 'admin-login');
+      v = new View(res, 'admin-login');
       v.render({
         title: 'Please login'
       });
@@ -29,6 +39,18 @@ module.exports = BaseController.extend({
       req.body.username === this.username &&
       req.body.password === this.password
     );
+  },
+  handleFileUpload: function(req) {
+	if(!req.files || !req.files.picture || !req.files.picture.name) {
+        return req.body.currentPicture || '';
+    }
+    var data = fs.readFileSync(req.files.picture.path);
+    var fileName = req.files.picture.name;
+    var uid = crypto.randomBytes(10).toString('hex');
+    var dir = __dirname + "/../public/uploads/" + uid;
+    fs.mkdirSync(dir, '0777');
+    fs.writeFileSync(dir + "/" + fileName, data);
+    return '/uploads/' + uid + "/" + fileName;
   }
 });
 
